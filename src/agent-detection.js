614625d61e09cca091778cc0a670342c421125e2
Óscar de la Mata <oscar@hanzo.es>
2017-04-25 10:01:57 +0200

    transformed to singleton

--- src/agent-detection.js
+++ src/agent-detection.js
@@ -1,142 +1,156 @@
-(function(){
+var agentDetection = (function(){
 
-  var ua = navigator.userAgent.toLowerCase();
+  var instance;
 
-  var isUA = function(text){
-    return ua.indexOf(text) > -1;
-  };
+  function init() {
+    var ua = navigator.userAgent.toLowerCase();
 
-  var isTouch = function() {
-    return ('ontouchstart' in window);
-  };
+    return {
+      isUA : function(text){
+        return ua.indexOf(text) > -1;
+      };
+      isTouch : function() {
+        return ('ontouchstart' in window);
+      };
+      /*
+      var isLegacyAndroid = function() {
+        return isUA("android 2") ||
+               isUA("android 3") ||
+               isUA("android 4");
+      };
 
-  var isLegacyAndroid = function() {
-    return isUA("android 2") ||
-           isUA("android 3") ||
-           isUA("android 4");
-  };
+      var isAndroid = function() {
+        return isUA("android") && !isLegacyAndroid();
+      };
 
-  var isAndroid = function() {
-    return isUA("android") && !isLegacyAndroid();
-  };
+      var isLegacyIOS = function() {
+        return isUA("iphone os 4") || isUA("cpu os 4") ||
+               isUA("iphone os 5") || isUA("cpu os 5") ||
+               isUA("iphone os 6") || isUA("cpu os 6") ||
+               isUA("iphone os 7") || isUA("cpu os 7");
+      };
 
-  var isLegacyIOS = function() {
-    return isUA("iphone os 4") || isUA("cpu os 4") ||
-           isUA("iphone os 5") || isUA("cpu os 5") ||
-           isUA("iphone os 6") || isUA("cpu os 6") ||
-           isUA("iphone os 7") || isUA("cpu os 7");
-  };
+      var isIOS = function() {
+        return (isUA("iphone os") || isUA("cpu os")) && !isLegacyIOS();
+      };
 
-  var isIOS = function() {
-    return (isUA("iphone os") || isUA("cpu os")) && !isLegacyIOS();
-  };
+      var isLegacyTouch = function() {
+        return  isLegacyAndroid() || isLegacyIOS();
+      };
 
-  var isLegacyTouch = function() {
-    return  isLegacyAndroid() || isLegacyIOS();
-  };
+      var isMobileIE = function() {
+        return isUA("iemobile");
+      };
 
-  var isMobileIE = function() {
-    return isUA("iemobile");
-  };
+      var isIE8 = function() {
+        return isUA("trident/4") && !isMobileIE();
+      };
 
-  var isIE8 = function() {
-    return isUA("trident/4") && !isMobileIE();
-  };
+      var isIE9 = function() {
+        return isUA("trident/5") && !isMSIE10() && !isMobileIE();
+      };
 
-  var isIE9 = function() {
-    return isUA("trident/5") && !isMSIE10() && !isMobileIE();
-  };
+      var isMSIE10 = function() {
+        return isUA("msie 10");
+      };
 
-  var isMSIE10 = function() {
-    return isUA("msie 10");
-  };
+      var isIE10 = function() {
+        return isUA("trident/6") || isMSIE10() && !isMobileIE();
+      };
 
-  var isIE10 = function() {
-    return isUA("trident/6") || isMSIE10() && !isMobileIE();
-  };
+      var isIE11 = function() {
+        return isUA("trident/7") && !isMobileIE();
+      };
 
-  var isIE11 = function() {
-    return isUA("trident/7") && !isMobileIE();
-  };
+      // Internet Explorer 6-11
+      var isIE = function() {
+        return false || !!document.documentMode;
+      };
 
-  // Internet Explorer 6-11
-  var isIE = function() {
-    return /*@cc_on!@*/false || !!document.documentMode;
-  };
+      // Edge 20+
+      var isIEEdge = function() {
+        return !isIE() && !!window.StyleMedia;
+      };
 
-  // Edge 20+
-  var isIEEdge = function() {
-    return !isIE() && !!window.StyleMedia;
-  };
+      var isWindows = function() {
+        return isUA("windows");
+      };
 
-  var isWindows = function() {
-    return isUA("windows");
-  };
+      // Firefox 1.0+
+      var isFirefox = function() {
+        return typeof window.InstallTrigger !== 'undefined';
+      };
 
-  // Firefox 1.0+
-  var isFirefox = function() {
-    return typeof window.InstallTrigger !== 'undefined';
-  };
+      //Opera 8.0+
+      var isOpera = function() {
+        return (!!window.opr && !!window.opr.addons) || !!window.opera || isUA(' OPR/');
+      };
 
-  //Opera 8.0+
-  var isOpera = function() {
-    return (!!window.opr && !!window.opr.addons) || !!window.opera || isUA(' OPR/');
-  };
+      var isIOSChrome = function() {
+        return isUA("CriOS");
+      };
 
-  var isIOSChrome = function() {
-    return isUA("CriOS");
-  };
+      var isChrome = function() {
+        return !!window.chrome && !!window.chrome.webstore;
+      };
 
-  var isChrome = function() {
-    return !!window.chrome && !!window.chrome.webstore;
-  };
+      // Safari 3.0+ "[object HTMLElementConstructor]"
+      var isSafari = function() {
+        return Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 ||
+        (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window.safari || window.safari.pushNotification);
+      };
 
-  // Safari 3.0+ "[object HTMLElementConstructor]"
-  var isSafari = function() {
-    return Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 ||
-    (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window.safari || window.safari.pushNotification);
-  };
+      //This clunky regex is taken directly from http://detectmobilebrowsers.com/
+      var isMobile = function() {
+        var stringCheck = (navigator.userAgent||navigator.vendor||window.opera);
+        return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(stringCheck) ||
+               /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(stringCheck.substr(0,4));
+      };
 
-  //This clunky regex is taken directly from http://detectmobilebrowsers.com/
-  var isMobile = function() {
-    var stringCheck = (navigator.userAgent||navigator.vendor||window.opera);
-    return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(stringCheck) ||
-           /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(stringCheck.substr(0,4));
-  };
+      var addClasses = function() {
+        var buffer = '';
 
-  var addClasses = function() {
-    var buffer = '';
+        UA = {
+          "ua-touch"          : isTouch(),
+          "ua-no-touch"       : !isTouch(),
+          "ua-legacy-android" : isLegacyAndroid(),
+          "ua-android"        : isAndroid(),
+          "ua-legacy-ios"     : isLegacyIOS(),
+          "ua-ios"            : isIOS(),
+          "ua-legacy-touch"   : isLegacyTouch(),
+          "ua-mobile-ie"      : isMobileIE(),
+          "ua-ie8"            : isIE8(),
+          "ua-ie9"            : isIE9(),
+          "ua-ie10"           : isIE10(),
+          "ua-ie11"           : isIE11(),
+          "ua-ie"             : isIE(),
+          "ua-windows"        : isWindows(),
+          "ua-firefox"        : isFirefox(),
+          "ua-opera"          : isOpera(),
+          "ua-ie-edge"        : isIEEdge(),
+          "ua-ios-chrome"     : isIOSChrome(),
+          "ua-chrome"         : isChrome(),
+          "ua-safari"         : isSafari(),
+          "ua-mobile"         : isMobile()
+        };
 
-    UA = {
-      "ua-touch"          : isTouch(),
-      "ua-no-touch"       : !isTouch(),
-      "ua-legacy-android" : isLegacyAndroid(),
-      "ua-android"        : isAndroid(),
-      "ua-legacy-ios"     : isLegacyIOS(),
-      "ua-ios"            : isIOS(),
-      "ua-legacy-touch"   : isLegacyTouch(),
-      "ua-mobile-ie"      : isMobileIE(),
-      "ua-ie8"            : isIE8(),
-      "ua-ie9"            : isIE9(),
-      "ua-ie10"           : isIE10(),
-      "ua-ie11"           : isIE11(),
-      "ua-ie"             : isIE(),
-      "ua-windows"        : isWindows(),
-      "ua-firefox"        : isFirefox(),
-      "ua-opera"          : isOpera(),
-      "ua-ie-edge"        : isIEEdge(),
-      "ua-ios-chrome"     : isIOSChrome(),
-      "ua-chrome"         : isChrome(),
-      "ua-safari"         : isSafari(),
-      "ua-mobile"         : isMobile()
-    };
-
-    for (var ua in UA) {
-      if (UA.hasOwnProperty(ua) && UA[ua]) {
-        buffer += ' '+ua+' ';
+        for (var ua in UA) {
+          if (UA.hasOwnProperty(ua) && UA[ua]) {
+            buffer += ' '+ua+' ';
+          }
+        }
+        document.documentElement.className += buffer;
       }
-    }
-    document.documentElement.className += buffer;
-  };
+      */
+    };
+  }
 
-}());
+  return {
+    getInstance: function () {
+      if ( !instance ) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+})();
